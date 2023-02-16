399db427dba9300cba99a952d933235d60a3ff39
kambhampatitejaswi <20bq1a0483@vvit.net>
2023-02-16 15:42:39 +0530

    Initial commit

---
+++ src/Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c=0;
+        int sum=0;
+        while(true) {
+            int a = scanner.nextInt();
+            if (a==0) {
+                break;
+            }
+            sum+=a;
+            c++;
+        }
+        System.out.println(sum/c);
+    }}
